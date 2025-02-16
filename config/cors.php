6712d269fdbb78ac1dd015f99d45c4f8f96202b7
YASSINOSRA <135080548+YASSINOSRA@users.noreply.github.com>
2025-02-16 19:38:30 +0100

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,9 +3,9 @@
 return [
 
     /*
-    |--------------------------------------------------------------------------
+    |----------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
+    |----------------------------------------------------------------------
     |
     | Here you may configure your settings for cross-origin resource sharing
     | or "CORS". This determines what cross-origin operations may execute
@@ -17,18 +17,25 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed methods
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://rec-production-2edb.up.railway.app', 
+    
+    ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [], // You can use patterns to allow specific domains
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [
+        'Content-Type', 
+        'Authorization', 
+        'X-Requested-With', // Add the headers you need for your application
+    ],
 
-    'exposed_headers' => [],
+    'exposed_headers' => [], // Expose additional headers if required
 
-    'max_age' => 0,
+    'max_age' => 0, // Cache duration for preflight request results in seconds
 
-    'supports_credentials' => false,
+    'supports_credentials' => true, // Enable this if you need cookies/authentication headers
 
 ];
